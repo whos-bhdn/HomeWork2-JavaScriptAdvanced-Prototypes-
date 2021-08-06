05be39cce47d0c0472943b1364bfce6f399435e4
whos-bhdn <anonimoos13@gmail.com>
2021-08-06 15:14:45 +0300

    done 2 methods

--- index.js
+++ index.js
@@ -76,3 +76,72 @@ CoffeeMachine.prototype.workInCafe = function (coffee, clients){
 // MydripCoffeeMachine.createCoffee(`LATTE`);
 // MyСarobСoffeeMachine.createCoffeeForAllMembersOfFamily(`latte`, 8)
 // MyCoffeeMachine.workInCafe('latte', 20)
+// --------------------------------------------------------------------------------------------
+class CoffeeMake {
+    constructor() {
+    }
+    on(){
+        console.log(`CofeeMachine is ON`)
+    }
+    off(){
+        console.log(`CofeeMachine is OFF`)
+    }
+}
+
+class DripCoffeeMachine extends CoffeeMake {
+    constructor(coffee) {
+        super()
+        this.coffee = coffee;
+    }
+    MakeCoffee(){
+        console.log(`Your ${this.coffee} is done`)
+    }
+}
+
+class CarobCoffeeMachine extends CoffeeMake{
+    constructor(coffee,members) {
+        super();
+        this.members = members;
+        this.coffee = coffee;
+    }
+    CoffeeForAllMembersOfFamily(){
+        if (this.members > 5){
+            console.log(`СarobСoffeeMachine is busy`);
+            setTimeout(() => {
+                console.log(`Your ${this.members} servings of ${this.coffee} are ready`);
+            }, (this.members*1000));
+        }
+        else if (members < 5 || members != 0){
+            console.log(`Your ${members} servings of ${coffee} are ready`);
+        }
+    }
+}
+
+class CoffeeMachine extends CoffeeMake {
+    constructor(coffee, clients) {
+        super();
+        this.coffee = coffee;
+        this.clients = clients;
+    }
+    workInCafe(){
+        if (this.clients < 10){
+            console.log(`Take your ${this.coffee}`)
+        }
+        else {
+            console.log(`CoffeeMachine doing your ${this.coffee}. PLease wait ${this.clients} clients are in front of you`)
+            setTimeout(() => {
+                console.log(`Your ${this.coffee} is done`)
+            }, (this.clients/2*1000))
+        }
+    }
+}
+// const MyCoffeeMake = new CoffeeMake();
+// const MyDripCoffeeMachine = new DripCoffeeMachine(`latte`);
+// const MyCarobCoffeeMachine = new CarobCoffeeMachine(`dark coffee`, 7);
+// const MyCoffeeMachine = new CoffeeMachine(`latte`, `15`);
+//
+// MyDripCoffeeMachine.MakeCoffee();
+// MyCoffeeMachine.on()
+// MyCoffeeMachine.workInCafe()
+// MyCarobCoffeeMachine.CoffeeForAllMembersOfFamily()
+
